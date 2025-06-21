00a2278d418ad8fd7ba9cc72c9e82bf9624598f9
hidroxide <nguyenhwdat@gmail.com>
2025-06-21 17:21:35 +0700

    Update carousel.jsx

--- web-client/components/productDetailPage/carousel.jsx
+++ web-client/components/productDetailPage/carousel.jsx
@@ -8,8 +8,17 @@ const CarouselFade = ({ imageList }) => {
         imageList.map((image, index) => {
           return (
             <div key={index}>
-              <div className="position-relative" style={{ paddingTop: "150%" }}>
-                <Image className="rounded" src={image} fill alt={index} />
+              <div
+                className="position-relative"
+                style={{ height: "600px", width: "100%" }}
+              >
+                <Image
+                  className="rounded"
+                  src={image}
+                  fill
+                  alt={`image-${index}`}
+                  style={{ objectFit: "contain" }}
+                />
               </div>
             </div>
           );
